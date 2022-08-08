27963df11fe9f935c542c3580a5a7aaf02d33b7d
Tilak Sharma <tilaksharma107@gmail.com>
2022-08-08 14:36:59 +0530

    nav header etc

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta name="description" content="Order online food &amp; grocery and get free delivery from your nearby restaurants, shops delivered online at your door step in Sikkim"/>
+<meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
+<link rel="canonical" href="https://www.kweekvery.com/" />
+<meta property="locale" content="en_US">
+<meta property="type" content="website">
+<meta property="title" content="Online Vegetables & Grocery Delivery in Sikkim">
+<meta property="description" content="Order online fruits, vegetables, meat &amp; grocery and get it delivered  at your door step in Sikkim">
+
+<meta property="site_name" content="Online Grocery Delivery in Sikkim">
+<meta property="updated_time" content="2021-08-21 T20:54:40+05:30">
+<meta property="image" content="https://kweekvery.com/public/kweekvery.png">
+<meta property="image:type" content="image/png">
+
+<title>Kweekvery @yield('title')</title>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+<!-- Styles -->
+<link href="/public/img/css/app.css" rel="stylesheet">
+<link href="/public/footer/css/main.css" rel="stylesheet">
+<link href="/public/img/css/new.css" rel="stylesheet">
+
+@yield('stylesheets')
